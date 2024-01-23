dd3bd280e8838acec10c656881a9d84473a8abcf
onefe20 <juyz730@tukorea.ac.kr>
2024-01-23 22:09:21 +0900

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//     res.setHeader("Content-Type", "text/html");
+//     res.end("OK");
+// });
+
+// server.listen("3000", () => console.log("OK 서버 시작"));
+const express = require("express");
+console.log(express)
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.set({"Content-Type" : "text/html; charset=utf-8"});
+    res.end("헬로 express");
+});
+
+app.listen(port, ()=>{
+    console.log(`START SERVER : use ${port}`);
+})
