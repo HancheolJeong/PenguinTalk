473ed61e408822e48c81bbfe7cff3077bfcd1d1e
HancheolJeong <juyz0730@gmail.com>
2024-02-06 00:55:18 +0900

    testing db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const path = require('path');
 
 var corsOptions = {
   origin: "http://localhost:3000"
@@ -15,10 +15,20 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'react-app/build')));
+
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
+// app.get("/", (req, res) => {
+//   res.senfFile(path.join(__dirname, 'react-app/build/index.html'));
+//   console.log(__dirname, 'react-app/build/index.html');
+// });
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'react-app/build/index.html'));
+  console.log(__dirname, 'react-app/build/index.html');
+})
+
+// require("./routes/routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
